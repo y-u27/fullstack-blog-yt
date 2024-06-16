d022ffff32ff47016e37c4282e642a394adac7f1
Atd27 <y518a480@gmail.com>
2024-06-16 16:21:31 +0900

    next.js13 youtube講座

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗しました");
+  }
+}
+
+// ブログの全記事取得API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
+    const posts = await prisma.post.findMany();
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// ブロブ投稿用API
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+    const { title, description } = await req.json();
+
+    await main();
+    const post = await prisma.post.create({ data: { title, description } })
+    return NextResponse.json({ message: "Success", post }, { status: 201});
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
